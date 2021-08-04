a43ccd3c0a58ad9d35ba145739085790bf9f544e
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-04 15:18:51 +0200

    added floor and ceil to math tools

--- src/__test__/math-tools.test.ts
+++ src/__test__/math-tools.test.ts
@@ -1,5 +1,23 @@
-import { MathTools } from '../math-tools';
+import { MathTools } from '../math-tools'
 
-test('comparing two numbers', () => {
-    expect(MathTools.min(4,5)).toEqual(4);
-  });
+test('returns the smaller number', () => {
+    expect(MathTools.min(4, 5)).toEqual(4)
+    expect(MathTools.min(8.4, 3.1)).toEqual(3.1)
+    expect(MathTools.min(8.5, 8.5)).toEqual(8.5)
+  })
+
+test('returns the bigger number', () => {
+  expect(MathTools.max(4, 5)).toEqual(5)
+  expect(MathTools.max(83.4, 3.4)).toEqual(83.4)
+  expect(MathTools.max(83.5, 83.5)).toEqual(83.5)
+})
+
+test('returns smallest integer bigger than argument', () => {
+   expect(MathTools.ceil(5.58)).toEqual(6)
+   expect(MathTools.ceil(5)).toEqual(5)
+ })
+
+ test('returns biggest integer smaller than argument', () => {
+  expect(MathTools.floor(5.52)).toEqual(5)
+  expect(MathTools.floor(5)).toEqual(5)
+})
--- src/math-tools.ts
+++ src/math-tools.ts
@@ -5,6 +5,15 @@ const MathTools = {
 
     min: (x: number, y: number) => {
         return (x <= y ? x : y)
+    },
+
+    ceil: (x: number) => {
+        if((x | 0) === x) return x
+        return (x | 0) + 1
+    },
+
+    floor : (x: number) => {
+        return (x | 0)
     }
 }
 
