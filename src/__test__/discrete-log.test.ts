ae0e4f6ce736f7bdb11099f7a3cc7b420e2f7495
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-10 14:44:04 +0200

    cleaned up

--- src/__test__/discrete-log.test.ts
+++ src/__test__/discrete-log.test.ts
@@ -1,6 +1,6 @@
 import {discreteLog} from "../discrete-log"
 
-test('returns the smallest primitive Root modulo argument (if there exist any)', () => {
+test('returns the discrete log of second argument given the base (first argument) modulo third argument)', () => {
     expect(discreteLog(7, 13, 22)).toEqual(3)
     expect(discreteLog(7, 17, 22)).toEqual(7)
     expect(discreteLog(2, 10, 11)).toEqual(5)
